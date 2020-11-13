217d812c0d91834ac254e29108bfaa2057e3f96e
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-13 16:47:32 +0000

    Printing all the cards

--- app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
+++ app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var board = Board()
+
+        for(card in board.cards!!){
+            println(card.toString())
+        }
     }
 }
