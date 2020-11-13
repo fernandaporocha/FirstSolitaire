00845e366a0f6c25de9464a28a0232589e36544a
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-13 17:30:57 +0000

    Adding Button to test the dealing of next stock card

--- app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
+++ app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
@@ -2,6 +2,7 @@ package ie.fernandarocha.firstsolitaire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,13 @@ class MainActivity : AppCompatActivity() {
 
         for(card in board.cards!!){
             println(card.toString())
+        }
+
+        val nextButton: Button = findViewById(R.id.next_button)
+        nextButton.setOnClickListener {
+            board.dealNextStock()
+            board.printStock()
+            board.printWaste()
         }
     }
 }
