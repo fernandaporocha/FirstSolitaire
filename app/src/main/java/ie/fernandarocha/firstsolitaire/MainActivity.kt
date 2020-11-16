5f2c7a4e0a53424981c9eb34ccab405523ed5725
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-16 17:46:09 +0000

    Loading images of the tableau piles

--- app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
+++ app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
@@ -26,24 +26,35 @@ class MainActivity : AppCompatActivity() {
         stockImage.setOnClickListener{ nextStockCard()}
         stockImage.setImageResource(R.drawable.purple_back)
 
+        loadTableauImages()
+
         var listener = View.OnTouchListener(function = { view, motionEvent ->
-
             if (motionEvent.action == MotionEvent.ACTION_MOVE) {
-
                 view.y = motionEvent.rawY - view.height/2
                 view.x = motionEvent.rawX - view.width/2
             }
-
             true
-
         })
 
-        findViewById<ImageView>(R.id.waste_image).setOnTouchListener(listener)
+        //findViewById<ImageView>(R.id.waste_image).setOnTouchListener(listener)
+         }
     }
 
     private fun nextStockCard(){
         board.dealNextStock()
         findViewById<ImageView>(R.id.waste_image).setImageResource(board.getWaste())
+    }
+
+    private fun loadTableauImages(){
+        findViewById<ImageView>(R.id.pile_0).setImageResource(if (board.tableau.piles[0].cards.isNotEmpty()) board.tableau.piles[0].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.pile_1).setImageResource(if (board.tableau.piles[1].cards.isNotEmpty()) board.tableau.piles[1].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.pile_2).setImageResource(if (board.tableau.piles[2].cards.isNotEmpty()) board.tableau.piles[2].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.pile_3).setImageResource(if (board.tableau.piles[3].cards.isNotEmpty()) board.tableau.piles[3].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.pile_4).setImageResource(if (board.tableau.piles[4].cards.isNotEmpty()) board.tableau.piles[4].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.pile_5).setImageResource(if (board.tableau.piles[5].cards.isNotEmpty()) board.tableau.piles[5].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.pile_6).setImageResource(if (board.tableau.piles[6].cards.isNotEmpty()) board.tableau.piles[6].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.waste_image).setImageResource(board.getWaste())
+
     }
 
 }
