06c4f3e394a9f77a89e51b24ac290cdd71fa2e44
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-16 17:40:43 +0000

    Adding the attribute currentPile to the class Card

--- app/src/main/java/ie/fernandarocha/firstsolitaire/Card.kt
+++ app/src/main/java/ie/fernandarocha/firstsolitaire/Card.kt
@@ -1,6 +1,6 @@
 package ie.fernandarocha.firstsolitaire
 
-class Card(val number: Int, val suit: Suit, val image: Int){
+class Card(val number: Int, val suit: Suit, val image: Int, var currentPile: Int = 7){
 
     override fun toString(): String {
         return this.number.toString() + this.suit;
