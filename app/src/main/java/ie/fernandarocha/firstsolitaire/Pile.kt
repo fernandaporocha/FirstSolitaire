f4d9fc120e1cbe39247850b4c8b91604487b7273
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-17 15:30:36 +0000

    Removing initialSize from Pile

--- app/src/main/java/ie/fernandarocha/firstsolitaire/Pile.kt
+++ app/src/main/java/ie/fernandarocha/firstsolitaire/Pile.kt
@@ -1,5 +1,5 @@
 package ie.fernandarocha.firstsolitaire
 
-class Pile(val position: Int, val initialSize: Int, val cards: ArrayList<Card>) {
+class Pile(val position: Int, val cards: ArrayList<Card>) {
 
 }
