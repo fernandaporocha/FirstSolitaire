c082c56d3d0984a97e1759d485db0be0c8bc40d3
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-17 15:34:11 +0000

    Changes to allow moving card to the Foundation

--- app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
+++ app/src/main/java/ie/fernandarocha/firstsolitaire/MainActivity.kt
@@ -32,18 +32,13 @@ class MainActivity : AppCompatActivity() {
         loadTableauImages()
         setTableauPilesEvents()
 
-        var listener = View.OnTouchListener(function = { view, motionEvent ->
-            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
-                view.y = motionEvent.rawY - view.height/2
-                view.x = motionEvent.rawX - view.width/2
-            }
-            true
-        })
+        loadFoundationImages()
+        setFoundationPilesEvents()
 
-        //findViewById<ImageView>(R.id.waste_image).setOnTouchListener(listener)
         findViewById<ImageView>(R.id.waste_image).setOnClickListener {
             println("clicou")
             selectCard(board.waste) }
+
     }
 
     private fun nextStockCard(){
@@ -51,36 +46,53 @@ class MainActivity : AppCompatActivity() {
         findViewById<ImageView>(R.id.waste_image).setImageResource(board.getWaste())
     }
 
-    private fun selectCard(pile:Pile){
-        if (pile.cards.isEmpty())
+    private fun selectCard(pile: Pile){
+        if (pile.position<8 && pile.cards.isEmpty() && selectedCard==null)
             return
 
         var cardImage: ImageView? = null
         when(pile.position){
-            0 ->  cardImage = findViewById<ImageView>(R.id.pile_0)
-            1 ->  cardImage = findViewById<ImageView>(R.id.pile_1)
-            2 ->  cardImage = findViewById<ImageView>(R.id.pile_2)
-            3 ->  cardImage = findViewById<ImageView>(R.id.pile_3)
-            4 ->  cardImage = findViewById<ImageView>(R.id.pile_4)
-            5 ->  cardImage = findViewById<ImageView>(R.id.pile_5)
-            6 ->  cardImage = findViewById<ImageView>(R.id.pile_6)
-            7 ->  cardImage = findViewById<ImageView>(R.id.waste_image)
+            0 -> cardImage = findViewById<ImageView>(R.id.pile_0)
+            1 -> cardImage = findViewById<ImageView>(R.id.pile_1)
+            2 -> cardImage = findViewById<ImageView>(R.id.pile_2)
+            3 -> cardImage = findViewById<ImageView>(R.id.pile_3)
+            4 -> cardImage = findViewById<ImageView>(R.id.pile_4)
+            5 -> cardImage = findViewById<ImageView>(R.id.pile_5)
+            7 -> cardImage = findViewById<ImageView>(R.id.waste_image)
+            8 -> cardImage = findViewById<ImageView>(R.id.foundation_0)
+            9 -> cardImage = findViewById<ImageView>(R.id.foundation_1)
+            10 -> cardImage = findViewById<ImageView>(R.id.foundation_2)
+            11 -> cardImage = findViewById<ImageView>(R.id.foundation_3)
         }
 
         println("selectCard")
         if (selectedCard!=null) {
             //verifica se clicou na carta que ja esta selecionada
-            if(selectedCard!!.equals(pile.cards?.last())){
-                if (cardImage != null) {
-                    cardImage.setBackgroundColor(Color.TRANSPARENT)
-                    cardImage.setPadding(0, 0, 0, 0)
+            if(pile.position<8){
+                if(pile.cards.isEmpty()&&selectedCard!!.number==13){
+                    moveCardTableau(pile, selectedCard!!)
+                }else if (pile.cards.isNotEmpty()) {
+                    if (selectedCard!!.equals(pile.cards?.last())) {
+                        if (cardImage != null) {
+                            cardImage.setBackgroundColor(Color.TRANSPARENT)
+                            cardImage.setPadding(0, 0, 0, 0)
+
+                        }
+                    }else {
+                        if(checkMarriageTableau(pile.cards.last(), selectedCard!!))
+                            moveCardTableau(pile, selectedCard!!)
+                    }
                 }
             }else{
                 println("else")
-                if(checkMarriageTableau(pile.cards.last(), selectedCard!!)){
+                //Trying to move to foundation
+                if(pile.position in 8..11){
+                    if(checkMarriageFoundation(pile.cards, selectedCard!!))
+                        moveCardToFoundation(pile, selectedCard!!)
+                }else if(checkMarriageTableau(pile.cards.last(), selectedCard!!)){
                     println("before move")
                     board.printTableau()
-                    moveCardTableau(pile.cards.last(), selectedCard!!)
+                    moveCardTableau(pile, selectedCard!!)
                     println("after move")
                     board.printTableau()
                 }
@@ -97,8 +109,9 @@ class MainActivity : AppCompatActivity() {
         println(pile.position)
     }
 
-    private fun moveCardTableau(to: Card, from: Card) {
-        var toPile: Int = to.currentPile
+    private fun moveCardTableau(to: Pile, from: Card) {
+        //if(to.cards.isEmpty()&&from.number==13)
+        var toPile: Int = to.position
         var fromPile:Int = from.currentPile
         println("MoveCard")
         println("toPile $toPile")
@@ -107,13 +120,34 @@ class MainActivity : AppCompatActivity() {
         println("from $from")
         from.currentPile = toPile
         println(board.tableau)
-        board.tableau.piles[to.currentPile].cards.add(from)
+        board.tableau.piles[to.position].cards.add(from)
         if (fromPile==7) {
             board.waste.cards.removeLast()
         }else {
             board.tableau.piles[fromPile].cards.removeLast()
         }
         loadTableauImages()
+
+    }
+
+    private fun moveCardToFoundation(to: Pile, from: Card) {
+        var toPile: Int = to.position
+        var fromPile:Int = from.currentPile
+        println("MoveCard")
+        println("toPile $toPile")
+        println("to $to")
+        println("fromPile $fromPile")
+        println("from $from")
+        from.currentPile = toPile
+        //println(board.tableau)
+        board.foundation.piles[to.position - 8].cards.add(from)
+        if (fromPile==7) {
+            board.waste.cards.removeLast()
+        }else {
+            board.tableau.piles[fromPile].cards.removeLast()
+        }
+        loadTableauImages()
+        loadFoundationImages()
 
     }
 
@@ -132,7 +166,30 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-    private fun checkSuit(suitTop: Suit, suitBottom:Suit):Boolean{
+    private fun checkMarriageFoundation(topCards: ArrayList<Card>, bottom: Card):Boolean {
+        println("CheckmarriageFoundation")
+        println(bottom)
+        if (topCards.isNotEmpty()){
+            if(topCards.last().suit==bottom.suit){
+                println("suit ok")
+                println(topCards.last().number)
+                println(bottom.number + 1)
+                if(topCards.last().number+1==bottom.number){
+                    println("casou")
+                    return true
+                }
+            }
+        }else{
+            if(bottom.number==1){
+                return true
+            }
+        }
+
+        println("Nao casou")
+        return false
+    }
+
+    private fun checkSuit(suitTop: Suit, suitBottom: Suit):Boolean{
         println("checksuit")
         println(suitTop)
         println(suitBottom)
@@ -165,4 +222,17 @@ class MainActivity : AppCompatActivity() {
         findViewById<ImageView>(R.id.pile_6).setOnClickListener { selectCard(board.tableau.piles[6]) }
     }
 
+    private fun loadFoundationImages(){
+        findViewById<ImageView>(R.id.foundation_0).setImageResource(if (board.foundation.piles[0].cards.isNotEmpty()) board.foundation.piles[0].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.foundation_1).setImageResource(if (board.foundation.piles[1].cards.isNotEmpty()) board.foundation.piles[1].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.foundation_2).setImageResource(if (board.foundation.piles[2].cards.isNotEmpty()) board.foundation.piles[2].cards.last().image else R.drawable.blue_back)
+        findViewById<ImageView>(R.id.foundation_3).setImageResource(if (board.foundation.piles[3].cards.isNotEmpty()) board.foundation.piles[3].cards.last().image else R.drawable.blue_back)
+    }
+
+    private fun setFoundationPilesEvents(){
+        findViewById<ImageView>(R.id.foundation_0).setOnClickListener { selectCard(board.foundation.piles[0]) }
+        findViewById<ImageView>(R.id.foundation_1).setOnClickListener { selectCard(board.foundation.piles[1]) }
+        findViewById<ImageView>(R.id.foundation_2).setOnClickListener { selectCard(board.foundation.piles[2]) }
+        findViewById<ImageView>(R.id.foundation_3).setOnClickListener { selectCard(board.foundation.piles[3]) }
+    }
 }
