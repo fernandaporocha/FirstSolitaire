6843a49e9fed58c6e3d9dfe3b5e5423c6070b249
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-18 08:50:58 +0000

    Adding method to return the current total of cards in the tableau

--- app/src/main/java/ie/fernandarocha/firstsolitaire/Tableau.kt
+++ app/src/main/java/ie/fernandarocha/firstsolitaire/Tableau.kt
@@ -1,4 +1,12 @@
 package ie.fernandarocha.firstsolitaire
 
 class Tableau(val piles: ArrayList<Pile>) {
+    fun getSize():Int{
+        var i:Int =0;
+        for (pile:Pile in this.piles){
+            if (pile.cards!=null)
+                i+=pile.cards.size
+        }
+        return i
+    }
 }
